478e9b360994b441df8a6bb3024517f058a4bb26
xiaominglol <474345633@qq.com>
2019-12-09 17:21:12 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
 // 导入组件库
 import ElementUI from 'element-ui';
 // 导入组件相关样式
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 // vue.config.js
 module.exports = {
   devServer: {
-    port: 8888
+    port: 8888,
+    // 反向代理配置
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8001',
+        ws: true,
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+    }
   }
 };
