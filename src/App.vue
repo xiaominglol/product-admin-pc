2c6d65e432e1a022595cb46d52188ace9b8e7567
xiaominglol <474345633@qq.com>
2020-01-08 16:04:11 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="app">
-<!--    <img alt="Vue logo" src="./assets/logo.png">-->
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-      <Login msg="Welcome to Your Vue.js App"/>
+      <router-view/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Login from './components/Login.vue'
 
 export default {
-  // name: 'app',
-  components: {
-    // HelloWorld,
-    Login
-  }
+    name: 'app'
 }
 </script>
 
